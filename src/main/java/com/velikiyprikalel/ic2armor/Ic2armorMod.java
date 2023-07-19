f4c9fd365e58f088b058b513055ef688a099ec54
Alex Poklonsky <jispar@mail.ru>
2023-07-19 10:10:34 +0300

    Init modules for mod.

--- src/main/java/com/velikiyprikalel/ic2armor/Ic2armorMod.java
+++ src/main/java/com/velikiyprikalel/ic2armor/Ic2armorMod.java
@@ -1,17 +1,20 @@
-package com.example.examplemod;
+package com.velikiyprikalel.ic2armor;
+
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.Logger;
+
+import com.velikiyprikalel.ic2armor.init.ItemInit;
 
 import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+@Mod(modid = Ic2armorMod.MODID, name = Ic2armorMod.NAME, version = Ic2armorMod.VERSION)
+public class Ic2armorMod {
+    public static final String MODID = "ic2armor";
+    public static final String NAME = "Industrial craft 2 Armor";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -25,7 +28,7 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        ItemInit.registerItems();
     }
 }

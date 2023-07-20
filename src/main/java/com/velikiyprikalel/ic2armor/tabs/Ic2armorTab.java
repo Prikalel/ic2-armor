d8e7c6dbcddd1e449d337315712e3654d7fd66ac
Alex Poklonsky <jispar@mail.ru>
2023-07-20 06:17:08 +0300

    Fix tab.

--- src/main/java/com/velikiyprikalel/ic2armor/tabs/Ic2armorTab.java
+++ src/main/java/com/velikiyprikalel/ic2armor/tabs/Ic2armorTab.java
@@ -1,20 +1,20 @@
 package com.velikiyprikalel.ic2armor.tabs;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class Ic2armorTab extends CreativeTabs {
 
-    public Ic2armorTab() 
-	{
+    public Ic2armorTab() {
 		super("creativetab");
 		this.setBackgroundImageName("creativetab.png");
 	}
 
     @Override
     public ItemStack getTabIconItem() {
-        // TODO Set item to show + add tab translation.
-        return new ItemStack(ItemInit.OBSIDIAN_INGOT);
+        // TODO: add tab translation.
+        return new ItemStack(Items.IRON_INGOT);
     }
     
 }

54b8d9a9ee102073232c70be592eb911a824eb22
Alex Poklonsky <jispar@mail.ru>
2023-07-31 23:34:02 +0300

    Increase version.

--- src/main/java/com/velikiyprikalel/ic2armor/Ic2armorMod.java
+++ src/main/java/com/velikiyprikalel/ic2armor/Ic2armorMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 public class Ic2armorMod {
     public static final String MOD_ID = "ic2armor";
     public static final String NAME = "Industrial craft 2 Armor";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static final CreativeTabs MOD_TAB = new Ic2armorTab();
 
